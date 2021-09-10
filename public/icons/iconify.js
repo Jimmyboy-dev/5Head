817c2b9af66a228f43b72ab2a2a1d127fde0a140
Jimmy Vrana <devJimmyboy@gmail.com>
2021-09-09 23:08:20 -0500

    Delete public/icons directory

--- public/icons/iconify.js
+++
@@ -1,7 +0,0 @@
-import { addCollection } from "@iconify/react"
-const bundles = import.meta.glob("/icons/*.json")
-for (const bundle in bundles) {
-  bundles[bundle]().then((mod) => {
-    addCollection(mod)
-  })
-}
